32e264d761a317b29ab0c22f456b5782c2cfd093
shadyhod <clownclowed@gmail.com>
2019-11-14 12:02:14 +0100

    Change port server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ import { resolvers, typeDefs } from './schema'
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || '4000').then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
 })
