3d97f4f6fa65c8f55e7fce7c5f20216b6339eaca
shadyhod <clownclowed@gmail.com>
2019-11-14 12:09:32 +0100

    Playground production available

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers, typeDefs } from './schema'
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  introspection: true,
+  playground: true,
+  typeDefs,
+  resolvers,
+})
 
 server.listen(process.env.PORT || '4000').then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
