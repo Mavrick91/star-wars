f7005774f27659d179698dfa48d34d1f1ba5b67b
shadyhod <clownclowed@gmail.com>
2019-11-24 21:14:19 +0100

    Remove unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@
 
 import { ApolloProvider } from '@apollo/react-hooks'
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
-import { createHttpLink } from 'apollo-link-http'
 import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
 import Routes from 'app/routes'
 import GlobalStyle, { theme } from 'app/style'
@@ -14,17 +13,7 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData,
 })
 
-const { NODE_ENV, REACT_APP_GRAPHQL_URI } = process.env
-const isNotProduction = NODE_ENV !== 'production'
-
-const uri = isNotProduction
-  ? 'http://localhost:4000/graphql'
-  : REACT_APP_GRAPHQL_URI
-const httpLink = createHttpLink({ uri })
-
-console.log('uri', uri)
 const client = new ApolloClient({
-  link: httpLink,
   cache: new InMemoryCache({
     fragmentMatcher,
   }),
